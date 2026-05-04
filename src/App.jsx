6263602ff8caaeac1182d316ad543c849201edc8
Ivan <tutktoto05@gmail.com>
2026-05-04 21:04:25 +0300

    v0.61, change text

--- src/App.jsx
+++ src/App.jsx
@@ -105,7 +105,7 @@ function App() {
       <footer>
         <div className="footerText">
           <p>© 2026 Jhn</p>
-          <p>JS online Editor v0.6</p>
+          <p>JS online Editor v0.61</p>
           <p><span>Lines now: {code.split("\n").length}</span></p>
         </div>
       </footer>
